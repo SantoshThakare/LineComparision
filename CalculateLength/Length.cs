b6a48a12c9fecccf58c9814925b4c7f0fca96f9e
SantoshThakare <santoshthakare50@gmail.com>
2022-01-28 16:21:34 +0530

    [Santosh]: folder added

---
+++ CalculateLength/Length.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLength
+{
+    public class Length
+    {
+        public void len()
+        {
+            int a1, a2, b1, b2;
+            double ans;
+
+            Console.WriteLine("Enter length point number (a1, b1)=");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter length point number (a2, b2)=");
+            a2 = Convert.ToInt32(Console.ReadLine());
+            b2 = Convert.ToInt32(Console.ReadLine());
+            ans = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            Console.WriteLine(" The length is : " + ans);
+        }
+    }
+}
---
+++ CalculateLength/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculateLength;
+
+Console.WriteLine("Hello, World!");
+Length next = new Length();
+next.len();
